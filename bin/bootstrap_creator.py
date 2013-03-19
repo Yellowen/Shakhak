0f6670752b8178db69bd69d55893eea085352422
Sameer Rahmani <lxsameer@gnu.org>
2013-03-19 11:04:06 +0330

    bootstrap script updated

--- bin/bootstrap_creator.py
+++ bin/bootstrap_creator.py
@@ -6,6 +6,6 @@ output = virtualenv.create_bootstrap_script(textwrap.dedent("""
 import os, subprocess
 def after_install(options, home_dir):
     subprocess.call([join(home_dir, 'bin', 'pip'),
-                     'install', 'ipython', 'django', 'pymongo', 'sphinx', 'tastypie'])
+                     'install', 'ipython', 'django', 'pymongo', 'sphinx', 'tastypie', 'vanda'])
 """))
 f = open('bootstrap.py', 'w').write(output)
